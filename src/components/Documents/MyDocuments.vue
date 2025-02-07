a99f9c27bf4f676c5d591527f9d24545b2fdda6f
liuzl.fnst <liuzl.fnst@fujitsu.com>
2025-02-07 10:14:27 +0800

    上传我的文档，我的项目和微搜等页面

--- src/components/Documents/MyDocuments.vue
+++ src/components/Documents/MyDocuments.vue
@@ -1,13 +1,174 @@
 <script setup>
 import { ref } from "vue";
 import { useRouter } from "vue-router";
+import CardTitle from "../Common/CardTitle.vue";
 const router = useRouter();
 
 defineProps({});
 
 const count = ref(0);
+const tableData = [
+  {
+    date: "2016-05-04",
+    name: "Aleyna Kutzner",
+    address: "Lohrbergstr. 86c, Süd Lilli, Saarland",
+  },
+  {
+    date: "2016-05-03",
+    name: "Helen Jacobi",
+    address: "760 A Street, South Frankfield, Illinois",
+  },
+  {
+    date: "2016-05-02",
+    name: "Brandon Deckert",
+    address: "Arnold-Ohletz-Str. 41a, Alt Malinascheid, Thüringen",
+  },
+  {
+    date: "2016-05-01",
+    name: "Margie Smith",
+    address: "23618 Windsor Drive, West Ricardoview, Idaho",
+  },
+  {
+    date: "2016-05-04",
+    name: "Aleyna Kutzner",
+    address: "Lohrbergstr. 86c, Süd Lilli, Saarland",
+  },
+  {
+    date: "2016-05-03",
+    name: "Helen Jacobi",
+    address: "760 A Street, South Frankfield, Illinois",
+  },
+  {
+    date: "2016-05-02",
+    name: "Brandon Deckert",
+    address: "Arnold-Ohletz-Str. 41a, Alt Malinascheid, Thüringen",
+  },
+  {
+    date: "2016-05-01",
+    name: "Margie Smith",
+    address: "23618 Windsor Drive, West Ricardoview, Idaho",
+  },
+  {
+    date: "2016-05-04",
+    name: "Aleyna Kutzner",
+    address: "Lohrbergstr. 86c, Süd Lilli, Saarland",
+  },
+  {
+    date: "2016-05-03",
+    name: "Helen Jacobi",
+    address: "760 A Street, South Frankfield, Illinois",
+  },
+  {
+    date: "2016-05-02",
+    name: "Brandon Deckert",
+    address: "Arnold-Ohletz-Str. 41a, Alt Malinascheid, Thüringen",
+  },
+  {
+    date: "2016-05-01",
+    name: "Margie Smith",
+    address: "23618 Windsor Drive, West Ricardoview, Idaho",
+  },
+  {
+    date: "2016-05-04",
+    name: "Aleyna Kutzner",
+    address: "Lohrbergstr. 86c, Süd Lilli, Saarland",
+  },
+  {
+    date: "2016-05-03",
+    name: "Helen Jacobi",
+    address: "760 A Street, South Frankfield, Illinois",
+  },
+  {
+    date: "2016-05-02",
+    name: "Brandon Deckert",
+    address: "Arnold-Ohletz-Str. 41a, Alt Malinascheid, Thüringen",
+  },
+  {
+    date: "2016-05-01",
+    name: "Margie Smith",
+    address: "23618 Windsor Drive, West Ricardoview, Idaho",
+  },
+];
 </script>
 
-<template>111111111</template>
+<template>
+  <el-row>
+    <el-col :span="24">
+      <el-card style="height: 100%" shadow="always">
+        <CardTitle title="我的文档" />
+        <div class="content">
+          <el-table
+            :data="tableData"
+            style="width: 100%; height: calc(100% - 50px)"
+          >
+            <el-table-column type="selection" width="55" />
+            <el-table-column label="类型">
+              <template #default="scope">{{ scope.row.date }}</template>
+            </el-table-column>
+            <el-table-column property="类型名" label="类型名" width="120" />
+            <el-table-column
+              property="创建时间"
+              label="创建时间"
+              width="240"
+              show-overflow-tooltip
+            />
+            <el-table-column property="address" label="修改时间" />
+            <el-table-column property="address" label="附件数" />
+            <el-table-column property="address" label="状态" />
+            <el-table-column fixed="right" label="操作" min-width="60">
+              <template #default>
+                <el-button
+                  link
+                  type="primary"
+                  size="small"
+                  @click="handleClick"
+                >
+                  查看
+                </el-button>
+              </template>
+            </el-table-column>
+          </el-table>
+          <el-pagination
+            size="small"
+            background
+            layout="prev, pager, next"
+            :total="50"
+            class="mt-4"
+          />
+        </div>
+      </el-card>
+    </el-col>
+  </el-row>
+</template>
 
-<style scoped></style>
+<style scoped>
+.el-card {
+  width: 100%;
+}
+::v-deep .el-card__body {
+  height: calc(100% - 40px);
+}
+.el-row {
+  height: 100%;
+}
+.el-col {
+  padding: 10px;
+  height: 100%;
+}
+.top10 {
+  margin-top: 10px;
+}
+.content {
+  width: 100%;
+  height: calc(100% - 40px);
+  margin-top: 10px;
+  overflow: hidden;
+}
+
+.mt-4 {
+  position: absolute;
+  right: calc(50% - 100px);
+  bottom: 30px;
+  transform: translate(-50%, 0);
+}
+</style>
--- src/components/Search/Search.vue
+++ src/components/Search/Search.vue
@@ -5,13 +5,34 @@ const router = useRouter();
 
 defineProps({});
 
-const count = ref(0);
+const search = ref("");
 </script>
 
 <template>
-  搜索：<Input></Input>
+  <div class="search-main">
+    <div class="search-item">
+      <el-input v-model="search"></el-input>
+      <el-button type="primary">搜索</el-button>
+    </div>
+  </div>
 </template>
 
 <style scoped>
+.search-main {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
 
+/* .search-item {
+  display: flex;
+  justify-content: center;
+} */
+
+.el-input {
+  width: 300px;
+  margin-right: 5px;
+}
 </style>
