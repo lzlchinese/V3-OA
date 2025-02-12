5c762a058e76a4b79a9ede26368feb06fc3e9ad7
liuzl.fnst <liuzl.fnst@fujitsu.com>
2025-02-12 15:42:23 +0800

    修改新建申请的所有流程的弹框标题等

--- src/components/Process/CreateProcess/Permanent.vue
+++ src/components/Process/CreateProcess/Permanent.vue
@@ -5,6 +5,10 @@ const props = defineProps({
     type: Boolean,
     required: true,
   },
+  processLevel: {
+    type: String,
+    required: true,
+  },
 });
 
 const emit = defineEmits(["getPermanentform"]);
@@ -12,16 +16,89 @@ const emit = defineEmits(["getPermanentform"]);
 const Permanentform = ref({});
 const dialogVisible = ref(false);
 const ruleFormRef = ref();
+const level = ref("");
+const title = ref("");
+const rangeTimeLabel = ref("");
 const rules = ref({
-  area: [{ required: true, message: "请输入驻场地区", trigger: "blur" }],
-  project: [{ required: true, message: "请输入驻场项目", trigger: "blur" }],
+  area: [{ required: true, message: "请输入出差地区", trigger: "blur" }],
+  project: [{ required: true, message: "请输入出差项目", trigger: "blur" }],
   remark: [{ required: true, message: "请输入申请理由", trigger: "blur" }],
-  rangeTime: [{ required: true, message: "选择驻场时间", trigger: "change" }],
+  rangeDate: [{ required: true, message: "选择出差时间", trigger: "change" }],
+  rangeTime: [{ required: true, message: "选择出差时间", trigger: "change" }],
 });
 watch(
   () => props.dialogFormVisible,
   (val) => {
     dialogVisible.value = val;
+    level.value = props.processLevel;
+    if (level.value === "1-1") {
+      title.value = "出差申请";
+      rangeTimeLabel.value = "出差时间";
+    } else if (level.value === "1-2") {
+      title.value = "外出申请";
+      rangeTimeLabel.value = "外出时间";
+    } else if (level.value === "1-3") {
+      title.value = "考勤补签申请";
+      rangeTimeLabel.value = "补勤时间";
+    } else if (level.value === "1-4") {
+      title.value = "劳动合同续签申请";
+    } else if (level.value === "1-5") {
+      title.value = "离职申请";
+    } else if (level.value === "1-6") {
+      title.value = "转正定级申请";
+    } else if (level.value === "1-7") {
+      title.value = "资格认证考试申请";
+    } else if (level.value === "1-8") {
+      title.value = "远程办公申请";
+    } else if (level.value === "2-1") {
+      title.value = "工会会员登记表";
+    } else if (level.value === "2-2") {
+      title.value = "采购申请（工会专用）";
+    } else if (level.value === "2-3") {
+      title.value = "合同审批申请（工会专用）";
+    } else if (level.value === "2-4") {
+      title.value = "验收入库申请（工会专用）";
+    } else if (level.value === "2-5") {
+      title.value = "费用报销/汇款申请（工会专用）";
+    } else if (level.value === "3-1") {
+      title.value = "费用报销/汇款申请";
+    } else if (level.value === "3-2") {
+      title.value = "到票流程";
+    } else if (level.value === "3-3") {
+      title.value = "借款申请";
+    } else if (level.value === "3-4") {
+      title.value = "专利奖励申请";
+    } else if (level.value === "4-1") {
+      title.value = "名片制作申请";
+    } else if (level.value === "4-2") {
+      title.value = "车辆使用申请";
+    } else if (level.value === "4-3") {
+      title.value = "公司资料申请";
+    } else if (level.value === "4-4") {
+      title.value = "专项培训签证/就业签证申请";
+    } else if (level.value === "5-1") {
+      title.value = "服务开通申请";
+    } else if (level.value === "5-2") {
+      title.value = "办公用品申请";
+    } else if (level.value === "5-3") {
+      title.value = "设备归还申请";
+    } else if (level.value === "5-4") {
+      title.value = "设备信息更新申请";
+    } else if (level.value === "5-5") {
+      title.value = "设备领用申请";
+    } else if (level.value === "5-6") {
+      title.value = "设备更换申请";
+    } else if (level.value === "5-7") {
+      title.value = "设备带出申请";
+    } else if (level.value === "6-1") {
+      title.value = "电子签章制作申请";
+    } else if (level.value === "6-2") {
+      title.value = "合同变更/作废申请";
+    } else if (level.value === "6-3") {
+      title.value = "用章申请";
+    } else if (level.value === "6-4") {
+      title.value = "合同审批申请";
+    }
   },
   { immediate: true }
 );
@@ -33,8 +110,7 @@ const submitForm = async (ruleFormRef) => {
     if (valid) {
       emit("getPermanentform", Permanentform);
       emit("dialogVisible", false);
-      Permanentform.value = []
-      console.log("submit!");
+      Permanentform.value = [];
     } else {
       console.log("error submit!", fields);
     }
@@ -49,32 +125,38 @@ const closeDialog = async () => {
 <template>
   <el-dialog
     v-model="dialogVisible"
-    title="长驻申请"
+    :title="title"
     width="30%"
     @close="closeDialog()"
   >
     <el-form ref="ruleFormRef" :model="Permanentform" :rules="rules">
-      <el-form-item label="长驻区域" :label-width="formLabelWidth" prop="area">
+      <el-form-item
+        label="出差区域"
+        :label-width="formLabelWidth"
+        prop="area"
+        v-if="level === '1-1'"
+      >
         <el-input v-model="Permanentform.area" autocomplete="off" />
       </el-form-item>
       <el-form-item
-        label="驻场时间"
+        :label="rangeTimeLabel"
         :label-width="formLabelWidth"
-        prop="rangeTime"
+        prop="rangeDate"
+        v-if="level === '1-1' || level === '1-2' || level === '1-3'"
       >
         <el-date-picker
-          v-model="Permanentform.rangeTime"
-          type="daterange"
-          range-separator="To"
+          v-model="Permanentform.rangeDate"
+          type="datetimerange"
+          range-separator="至"
           start-placeholder="开始时间"
           end-placeholder="结束时间"
-          :size="size"
         />
       </el-form-item>
       <el-form-item
-        label="驻场项目"
+        label="出差项目"
         :label-width="formLabelWidth"
         prop="project"
+        v-if="level === '1-1'"
       >
         <el-input v-model="Permanentform.project" autocomplete="off" />
       </el-form-item>
--- src/main.js
+++ src/main.js
@@ -8,6 +8,8 @@ import * as echarts from "echarts";
 // 引入 Element Plus 和样式
 import ElementPlus from "element-plus";
 import "element-plus/dist/index.css";
+// 修改 Element Plus 的语言为中文
+import locale from "element-plus/es/locale/lang/zh-cn";
 // 引入 路由
 import router from "../router";
 // 导入全局样式文件
@@ -17,7 +19,7 @@ const app = createApp(App);
 const pinia = createPinia();
 
 app.use(pinia);
-app.use(ElementPlus);
+app.use(ElementPlus, { locale });
 app.use(router);
 // 使用组件
 app.component("e-charts", Echarts);
