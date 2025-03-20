cde7278166fa9129a7d64ff8485eb214ba84bda3
liuzhonglei <1761997923@qq.com>
2025-03-20 21:47:30 +0800

    上传我的卡片接口

--- src/components/Personnel/MyCard.vue
+++ src/components/Personnel/MyCard.vue
@@ -1,10 +1,26 @@
 <script setup>
-import { ref } from "vue";
+import { ref, onMounted } from "vue";
 import { useRouter } from "vue-router";
 import CardTitle from "../Common/CardTitle.vue";
+import axios from "../../axios/axios";
 const router = useRouter();
 
 defineProps({});
+const myInfo = ref({})
+const fetchData = async () => {
+  try {
+    const response = await axios.get("/users?name=刘忠磊");
+    myInfo.value = response[0]
+  } catch (error) {
+    console.error("myCard数据请求失败:", error);
+
+  }
+};
+
+// // 在组件挂载后发起请求
+onMounted(async () => {
+  await fetchData();
+});
 
 const count = ref(0);
 </script>
@@ -16,65 +32,35 @@ const count = ref(0);
         <CardTitle title="我的卡片" />
         <div class="content">
           <el-descriptions border>
-            <el-descriptions-item
-              :rowspan="4"
-              :width="140"
-              label="基本信息"
-              align="center"
-            >
-              <el-image
-                style="width: 100px; height: 100px"
-                src="https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png"
-              />
+            <el-descriptions-item :rowspan="4" :width="140" label="基本信息" align="center">
+              <el-image style="width: 100px; height: 100px"
+                src="https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png" />
             </el-descriptions-item>
-            <el-descriptions-item label="姓名">刘忠磊</el-descriptions-item>
-            <el-descriptions-item label="性别">男</el-descriptions-item>
-            <el-descriptions-item label="生日">1996-06-15</el-descriptions-item>
-            <el-descriptions-item label="民族">汉族</el-descriptions-item>
-            <el-descriptions-item label="籍贯">江苏</el-descriptions-item>
+            <el-descriptions-item label="姓名">{{ myInfo.name }}</el-descriptions-item>
+            <el-descriptions-item label="性别">{{ myInfo.sex }}</el-descriptions-item>
+            <el-descriptions-item label="生日">{{ myInfo.bathday }}</el-descriptions-item>
+            <el-descriptions-item label="民族">{{ myInfo.ethnicity }}</el-descriptions-item>
+            <el-descriptions-item label="籍贯">{{ myInfo.placeOfOrigin }}</el-descriptions-item>
             <el-descriptions-item label="住址">
-              江苏省南京市建邺区虹苑新寓
+              {{ myInfo.livingPlace }}
             </el-descriptions-item>
-            <el-descriptions-item label="个人电话"
-              >13062531032</el-descriptions-item
-            >
-            <el-descriptions-item label="个人电子邮件"
-              >1761997923@qq.com</el-descriptions-item
-            >
-            <el-descriptions-item
-              :rowspan="6"
-              :width="140"
-              label="公司信息"
-              align="center"
-            >
-              <el-image
-                style="width: 100px; height: 100px"
-                src="src/images/fnst.jpg"
-              />
+            <el-descriptions-item label="个人电话">{{ myInfo.phone }}</el-descriptions-item>
+            <el-descriptions-item label="个人电子邮件">{{ myInfo.email }}</el-descriptions-item>
+            <el-descriptions-item :rowspan="6" :width="140" label="公司信息" align="center">
+              <el-image style="width: 100px; height: 100px" src="src/images/fnst.jpg" />
             </el-descriptions-item>
-            <el-descriptions-item label="办公地点">南京</el-descriptions-item>
-            <el-descriptions-item label="所属分社地址"
-              >江苏省南京市雨花台区软件大道xxx号</el-descriptions-item
-            >
-            <el-descriptions-item label="岗位">开发工程师</el-descriptions-item>
-            <el-descriptions-item label="职务">开发担当</el-descriptions-item>
-            <el-descriptions-item label="职级"></el-descriptions-item>
-            <el-descriptions-item label="直接上司">xxxx</el-descriptions-item>
-            <el-descriptions-item label="座机号码">无</el-descriptions-item>
-            <el-descriptions-item label="电子邮件"
-              >liuzl.fnst@fujistu.com</el-descriptions-item
-            >
-            <el-descriptions-item label="办公区域"
-              >南京富士通大楼3楼西</el-descriptions-item
-            >
-            <el-descriptions-item label="入职时间"
-              >2021-04-01</el-descriptions-item
-            >
-            <el-descriptions-item label="资格认证"
-              >ServiceNow资格证，aws初级</el-descriptions-item
-            ><el-descriptions-item label="技能专长"
-              >vue，node，aws</el-descriptions-item
-            >
+            <el-descriptions-item label="办公地点">{{ myInfo.Officelocation }}</el-descriptions-item>
+            <el-descriptions-item label="岗位">{{ myInfo.post }}</el-descriptions-item>
+            <el-descriptions-item label="职务">{{ myInfo.position }}</el-descriptions-item>
+            <el-descriptions-item label="职级">{{ myInfo.rank }}</el-descriptions-item>
+            <el-descriptions-item label="直接上司">{{ myInfo.leader }}</el-descriptions-item>
+            <el-descriptions-item label="座机号码">{{ myInfo.landline }}</el-descriptions-item>
+            <el-descriptions-item label="电子邮件">{{ myInfo.Companyemail }}</el-descriptions-item>
+            <el-descriptions-item label="办公区域">{{ myInfo.officeArea }}</el-descriptions-item>
+            <el-descriptions-item label="入职时间">{{ myInfo.entryTime }}</el-descriptions-item>
+            <el-descriptions-item label="资格认证">{{ myInfo.qualification
+              }}</el-descriptions-item><el-descriptions-item label="技能专长">{{ myInfo.technical
+              }}</el-descriptions-item>
           </el-descriptions>
         </div>
       </el-card>
